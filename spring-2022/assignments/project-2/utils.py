32828238546cacaf4c3f03d533062a5b416e2a2c
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-16 20:40:11 +0000

    Adding tarfile member sanitization to extractall()

--- spring-2022/assignments/project-2/utils.py
+++ spring-2022/assignments/project-2/utils.py
@@ -81,7 +81,26 @@ def get_file(fname,
         if not os.path.exists(untar_fpath):
             print('Extracting file.')
             with tarfile.open(fpath) as archive:
-                archive.extractall(datadir)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(archive, datadir)
         return untar_fpath
 
     return fpath
